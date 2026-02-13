fc89a70fbce12708d1c39d84dd5421406ff70f54
Marek Chalupa <mchqwerty@gmail.com>
2026-02-13 11:42:41 +0100

    avoidance: update run.py

--- experiments/avoidance/run.py
+++ experiments/avoidance/run.py
@@ -3,52 +3,59 @@ from random import randrange
 
 import datetime
 from subprocess import Popen, PIPE, TimeoutExpired
-from os.path import dirname, basename, join, isfile, isdir
+from os.path import dirname, basename, join, abspath, isdir
 import os
-from os import listdir, access, X_OK
+from os import listdir, makedirs
 from sys import stderr, stdout
 from multiprocessing import Pool
 import signal
 
 import argparse
 
+SELFDIR = join(dirname(__file__))
+LOGFILE = join(SELFDIR, "log.txt")
+
 
 def errlog(*args):
-    with open(join(dirname(__file__), "log.txt"), "a") as logf:
+    with open(LOGFILE, "w") as logf:
         for a in args:
             print(a, file=logf)
 
 
-def run_monitor(trace, prp, args):
-    cmd = args.qsfo_cmd.split("")
+def run_monitor(arg):
+    trace, prp, args = arg
+    cmd = args.qsfo_cmd.split()
     cmd.append(prp)
     cmd.append(trace)
     cmd.append(f"--samp={args.sampling}")
     cmd.append(f"--horizon={args.horizon}")
-    cmd.append(f'--csv="{args.out}/{basename(trace)}')
+    cmd.append(f"--csv={args.out}/{basename(trace)}")
     cmd.append("--no-stdout")
 
     p = Popen(cmd, stderr=PIPE, stdout=PIPE, preexec_fn=os.setsid)
     try:
         out, err = p.communicate(timeout=args.timeout)
         if p.returncode != 0:
-            errlog(p, out, err)
+            errlog("------", " ".join(cmd), "------", out, "------", err)
     except TimeoutExpired:
         os.killpg(os.getpgid(p.pid), signal.SIGTERM)
         out, err = p.communicate(timeout=10)
-    return (trace, p.returncode, out, err)
+    return (trace, p.returncode, cmd, out, err)
 
 
-def get_params(traces, prp, args):
+def get_params(traces_dir, traces, prp, args):
     # yield trace, prp, args
     for t in traces:
-        yield t, prp, args
+        yield abspath(join(traces_dir, t)), prp, args
 
 
-def run(args):
-    print(
-        f"\033[1;34m[{datetime.datetime.now().time()}] Running using {args.j or 'automatic # of'} workers\n\033[0m",
-    )
+def run(prp, args):
+    print(f"\nMonitoring \033[0;32m{prp}\033[0m\n")
+    print(f" ... running using {args.j or 'automatic # of'} processes")
+    if args.j is None:
+        print(" ... (the number of used processes can be adjusted by parameter `-j`)")
+
+    print(f" ... It's {datetime.datetime.now().time()}")
     stdout.flush()
 
     print(f" ... finding CSV traces in `{args.traces}`")
@@ -56,8 +63,11 @@ def run(args):
     n_traces = len(traces_list)
     print(f" ... found {n_traces} traces")
 
+    if n_traces == 0:
+        raise RuntimeError(f"Found no traces in `{args.traces}`")
+
     n_random_traces: None | int = args.n_random_traces
-    if n_random_traces is not None:
+    if n_random_traces is not None and n_random_traces < n_traces:
         print(f" ... randomly selecting {n_random_traces} traces")
         selected_traces = [
             traces_list[randrange(0, n_traces)] for _ in range(n_random_traces)
@@ -65,30 +75,31 @@ def run(args):
     else:
         selected_traces = traces_list
 
-    # PRP 1
-    prp = f"t < {args.ignore_time} \or vx(t) < {args.vx_max}"
-
     verbose = args.verbose
 
     N = len(selected_traces)
     n = 0
 
-    print("Altogether,", N, "runs get executed\n")
+    print(" ... altogether,", N, "runs get executed")
     print("-------------------------------------")
 
     if isdir(args.out):
-        print("Storing outputs to directory (overwriting files that might be there)")
+        print(
+            f"Storing outputs to directory `{args.out}` (overwriting files that might be there)"
+        )
+        print("-------------------------------------")
+    else:
+        print(f" ... creating directory `{args.out}`")
+        makedirs(args.out, exist_ok=True)
         print("-------------------------------------")
 
-    with Pool(processes=args.j) as pool, open(args.out, "w") as out:
+    with Pool(processes=args.j) as pool:
         for result in pool.imap_unordered(
-            run_monitor, get_params(selected_traces, prp, args)
+            run_monitor, get_params(args.traces, selected_traces, prp, args)
         ):
-            trace, exitcode, out, err = result
+            trace, exitcode, cmd, out, err = result
             if exitcode != 0:
-                errlog("Failed running monitor:")
-                errlog(out)
-                errlog(err)
+                print(f"Failed running monitor, see `{LOGFILE}`", file=stderr)
 
             progress = 100 * (n / N)
             if verbose:
@@ -99,7 +110,9 @@ def run(args):
             n += 1
 
         print("\nAll done!")
-        print("Results stored into", args.out)
+        print(f" ... It's {datetime.datetime.now().time()}")
+        print("-------------------------------------")
+        print(f"Results stored into `{args.out}`")
 
 
 def parse_cmd():
@@ -109,7 +122,7 @@ def parse_cmd():
         "--out",
         help="Name of the output file. Default is 'out.csv'",
         action="store",
-        default="out.csv",
+        default=join(SELFDIR, "results"),
     )
     parser.add_argument(
         "--verbose",
@@ -119,9 +132,7 @@ def parse_cmd():
     )
 
     parser.add_argument(
-        "--traces",
-        help="Directory with traces",
-        action="store",
+        "--traces", help="Directory with traces", action="store", required=True
     )
     parser.add_argument(
         "--t_s",
@@ -167,6 +178,13 @@ def parse_cmd():
         type=int,
         default=None,
     )
+    parser.add_argument(
+        "--n-random-traces",
+        help="Select a subset of `n` random traces from the given traces",
+        action="store",
+        type=int,
+        default=None,
+    )
 
     parser.add_argument(
         "--qsfo-cmd",
@@ -176,51 +194,16 @@ def parse_cmd():
     )
 
     args = parser.parse_args()
-
-    if isinstance(args.traces_lens, str):
-        args.traces_lens = list(map(int, args.traces_lens.split(",")))
-    if isinstance(args.traces_nums, str):
-        args.traces_nums = list(map(int, args.traces_nums.split(",")))
-    if isinstance(args.bits, str):
-        args.bits = list(map(int, args.bits.split(",")))
-
-    args.monitors = args.monitors.split(",")
+    args.out = abspath(args.out)
 
     return args
-
-
-def shl_monitors(args):
-    for m in args.monitors:
-        if m.startswith("shl"):
-            yield m
 
 
 if __name__ == "__main__":
     args = parse_cmd()
 
-    problem = False
-    for mon in shl_monitors(args):
-        mon = join(f"{hnl_dir}/{mon}", "monitor")
-        if not (isfile(mon) and access(mon, X_OK)):
-            print(
-                f"Did not find sHL monitor ({mon}). Please run `'./generate-shl.sh` "
-                "first (you may need to modify the script to generate the right monitor).",
-                file=stderr,
-            )
-            problem = True
-
-    for bits in args.bits:
-        mon = join(f"{hnl_dir}/ehl-{bits}b", "monitor")
-        if not (isfile(mon) and access(mon, X_OK)):
-            print(
-                f"Did not find eHL monitor for {bits} bits. Please run `'./generate-ehl.sh "
-                f"{bits}b'` first.",
-                file=stderr,
-            )
-            problem = True
-
-    if problem:
-        exit(1)
+    # PRP 1
+    prp = f"t < {args.t_s} \\or vx(t) < {args.v_max}"
 
     # do it!
-    run(args)
+    run(prp, args)
